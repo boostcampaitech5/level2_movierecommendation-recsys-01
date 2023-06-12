945635b9c3c5df60b5784a16291c270042bd475b
junwon <jjuny9798@gmail.com>
2023-06-12 07:56:19 +0000

    feat: split train valid dataset
    Add metric to evaluate the performance of model
    
     #5

--- src/EASE/train.py
+++ src/EASE/train.py
@@ -2,7 +2,10 @@ import os
 import yaml
 import pickle
 import pandas as pd
+import numpy as np
+from utils.train_valid import train_valid_split
 from utils.preprocess import create_matrix_and_mappings
+from utils.metric import recall_at_10
 from model.ease import EASE
 
 def main():
@@ -16,10 +19,22 @@ def main():
     print('###############')
     print('Loading data.. \n')
     train_df = pd.read_csv(os.path.join(config['data_path'], 'train_ratings.csv'))
+    
+    print('###############')
+    print('Splitting train / valid..\n')
+    train_data, valid_data = train_valid_split(train_df, num_seq=4, num_ran=6)
+    
+    # Create a folder to store the datasets
+    # folder_path =config['data_path']
+    # os.makedirs(folder_path, exist_ok=True)
 
+    # Save train data and valid data as .csv files
+    # train_data.to_csv(os.path.join(folder_path, 'train_data.csv'), index=False)
+    # valid_data.to_csv(os.path.join(folder_path, 'valid_data.csv'), index=False)
+    
     print('###############')
     print('Preprocessing..\n')
-    X, _, _ = create_matrix_and_mappings(train_df)
+    X, index_to_user, index_to_item = create_matrix_and_mappings(train_data)
 
     print('###############')
     print('Model: EASE\n')
@@ -28,7 +43,29 @@ def main():
     print('###############')
     print('Training..\n')
     model.train(X)
+    
+    print('###############')
+    print('Predicting.. \n')
+    result = model.forward(X[:, :])
+    #To remove information that has already been evaluated
+    result[X.nonzero()] = -np.inf
 
+    #Extract top 10
+    recommend_list=[]
+    for i in range(len(result)):
+        sorted_indices = np.argsort(-result[i])
+        for j in sorted_indices.tolist():
+            for k in range(10):
+                recommend_list.append((index_to_user[i], index_to_item[j[k]]))
+                
+    pred_df = pd.DataFrame(recommend_list, columns=['user', 'item'])
+
+    # Evaluate Recall@10 performance
+    recall_10 = recall_at_10(true_df=valid_data, pred_df= pred_df)
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+    print("Recall@10:", recall_10)
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n')
+    
     print('###############')
     print('Saving model..\n')
     save_folder = config['save_model_path']
@@ -42,3 +79,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
