9a97bdc8d9301e5f0aee9bbe12005d8a5e22ebff
junwon <jjuny9798@gmail.com>
2023-06-14 08:29:29 +0000

    chore: add parameter
    change README.md
    
     #5

--- src/EASE/train.py
+++ src/EASE/train.py
@@ -10,96 +10,103 @@ from utils.metric import recall_at_10
 from utils.utils import korea_date_time
 from model.ease import EASE
 
+
 def main():
     # Path to the ease.yaml file
-    config_path = 'config/ease.yaml'
+    config_path = "config/ease.yaml"
 
     # Load the contents of the YAML file
-    with open(config_path, 'r') as file:
+    with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
-    # WandB init
-    wandb.login(key="87f939bb2c7ea08b8246da620fcde06361132916")
-   
-    project_name = "MovieRec"
-    user_name = "junwon"
-    model_name = "EASE"
-    date_time = korea_date_time()
-    
-    wandb_args = {'_lambda': config['_lambda'], 'scale': config['scale']}
+    print("###############")
+    print("Loading data.. \n")
+    train_df = pd.read_csv(os.path.join(config["data_path"], "train_ratings.csv"))
 
-    wandb.init(
-    project=project_name,
-    name=f"{user_name}-{model_name}-{date_time}",
-    config=wandb_args
-    )
-    
-    print('###############')
-    print('Loading data.. \n')
-    train_df = pd.read_csv(os.path.join(config['data_path'], 'train_ratings.csv'))
-    
-    print('###############')
-    print('Splitting train / valid..\n')
-    train_data, valid_data = train_valid_split(train_df, num_seq=4, num_ran=6)
-    
-    # Create a folder to store the datasets
-    # folder_path =config['data_path']
-    # os.makedirs(folder_path, exist_ok=True)
+    if config["cv"]:
+        # WandB init
+        wandb.login(key=config["wandb_key"])
 
-    # Save train data and valid data as .csv files
-    # train_data.to_csv(os.path.join(folder_path, 'train_data.csv'), index=False)
-    # valid_data.to_csv(os.path.join(folder_path, 'valid_data.csv'), index=False)
-    
-    #train with train_data for cv
-    print('###############')
-    print('Preprocessing..\n')
-    X_cv, index_to_user, index_to_item = create_matrix_and_mappings(train_data,config['scale'])
+        project_name = "MovieRec"
+        user_name = config["user_name"]
+        model_name = "EASE"
+        date_time = korea_date_time()
 
-    print('###############')
-    print('Model: EASE\n')
-    model_cv = EASE(_lambda=config['_lambda'])
-    
-    print('###############')
-    print('Training..\n')
-    model_cv.train(X_cv)
-    
-    print('###############')
-    print('Predicting.. \n')
-    result = model_cv.forward(X_cv[:, :])
-    #To remove information that has already been evaluated
-    result[X_cv.nonzero()] = -np.inf
+        wandb_args = {
+            "_lambda": config["_lambda"],
+            "scale": config["scale"],
+            "cv": config["cv"],
+        }
 
-    #Extract top 10
-    recommend_list=[]
-    for i in range(len(result)):
-        sorted_indices = np.argsort(-result[i])
-        for j in sorted_indices.tolist():
-            for k in range(10):
-                recommend_list.append((index_to_user[i], index_to_item[j[k]]))
-                
-    pred_df = pd.DataFrame(recommend_list, columns=['user', 'item'])
+        wandb.init(
+            project=project_name,
+            name=f"{user_name}-{model_name}-{date_time}",
+            config=wandb_args,
+        )
 
-    # Evaluate Recall@10 performance
-    recall_10 = recall_at_10(true_df=valid_data, pred_df= pred_df)
-    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    print("Recall@10:", recall_10)
-    wandb.log({"Recall@10": recall_10})
-    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n')
-    
+        print("###############")
+        print("Splitting train / valid..\n")
+        train_data, valid_data = train_valid_split(train_df, num_seq=4, num_ran=6)
+
+        # Create a folder to store the datasets
+        # folder_path =config['data_path']
+        # os.makedirs(folder_path, exist_ok=True)
+
+        # Save train data and valid data as .csv files
+        # train_data.to_csv(os.path.join(folder_path, 'train_data.csv'), index=False)
+        # valid_data.to_csv(os.path.join(folder_path, 'valid_data.csv'), index=False)
+
+        print("###############")
+        print("Evaluating with valid data..\n")
+        X_cv, index_to_user, index_to_item = create_matrix_and_mappings(
+            train_data, config["scale"]
+        )
+        model_cv = EASE(_lambda=config["_lambda"])
+        model_cv.train(X_cv)
+        result = model_cv.forward(X_cv[:, :])
+        result[X_cv.nonzero()] = -np.inf
+
+        # Extract top 10
+        recommend_list = []
+        for i in range(len(result)):
+            sorted_indices = np.argsort(-result[i])
+            for j in sorted_indices.tolist():
+                for k in range(10):
+                    recommend_list.append((index_to_user[i], index_to_item[j[k]]))
+
+        pred_df = pd.DataFrame(recommend_list, columns=["user", "item"])
+
+        # Evaluate Recall@10 performance
+        recall_10 = recall_at_10(true_df=valid_data, pred_df=pred_df)
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        print("Recall@10:", recall_10)
+        wandb.log({"Recall@10": recall_10})
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+
     # train with total data
-    X, _, _ = create_matrix_and_mappings(train_df,config['scale'])
-    model = EASE(_lambda=config['_lambda'])
+    print("###############")
+    print("Preprocessing..\n")
+    X, _, _ = create_matrix_and_mappings(train_df, config["scale"])
+
+    print("###############")
+    print("Model: EASE\n")
+    model = EASE(_lambda=config["_lambda"])
+
+    print("###############")
+    print("Training..\n")
     model.train(X)
-    print('###############')
-    print('Saving model..\n')
-    save_folder = config['save_model_path']
+
+    print("###############")
+    print("Saving model..\n")
+    save_folder = config["save_model_path"]
     os.makedirs(save_folder, exist_ok=True)
 
     with open(save_folder + "/model_params.pkl", "wb") as f:
         pickle.dump(model, f)
 
-    print('###############')
-    print('Saved successfully')
+    print("###############")
+    print("Saved successfully")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
