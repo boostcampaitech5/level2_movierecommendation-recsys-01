f19b63be43cb23569209dac6ef0ccfdc1a6d8ffd
junwon <jjuny9798@gmail.com>
2023-06-18 13:14:04 +0000

    refactor: uniformize the code format
    File path unification and script file creation for efficient code use.
    Update README file for additional explanation.
    
     #13

--- src/Ensemble/README.md
+++ src/Ensemble/README.md
@@ -2,29 +2,68 @@
 
 추천 결과의 유사도를 구하는 코드와 Top-K 앙상블을 진행하는 코드를 구현하였습니다.
 
-# Files
+## Files
 
-get_similiarty.py - 추천한 아이템이 얼마나 겹치는지 %를 보여주는 코드입니다. 
+- get_similiarty.py
+    추천한 아이템이 얼마나 겹치는지 %를 보여주는 코드입니다. 
+<br>
 
-topk_ensemble.py - Soft Voting 방식을 사용하여 추천 결과를 앙상블하는 코드입니다.
+- topk_with_weight.py
+    Soft Voting 방식을 사용하여 추천 결과를 앙상블하는 코드입니다.
+<br>
 
+-  topk_without_weight.py
+    모델별로 상위의 아이템을 그대로 가져와 추천 결과를 앙상블하는 코드입니다.
+<br>
 
-# Directory Structure
+- split_by_interaction.py
+    훈련 데이터의 유저의 상호작용 횟수별로 유저를 나눠 다른 모델을 적용하는 코드입니다.
+<br>
+
+## Directory Structure
 
 submission - 유사도를 구할 submission.csv 파일을 저장하는 디렉토리입니다. (get_similiarity.py 코드에 사용됩니다)
-
+<br>
 for_ensemble - 앙상블을 진행할 submission.csv 파일을 저장하는 디렉토리입니다. (topk_ensemble.py 코드에 사용됩니다)
+<br><br>
 
-# How to run
+## How to run
 
 ```
 # Numpy, Pandas, Tqdm 라이브러리가 모두 설치되어 있는 가상환경을 실행합니다.
 # submission 디렉토리에 유사도를 구하고 싶은 csv파일을 저장합니다.
 # for_ensemble 디렉토리에 앙상블을 진행할 csv파일을 저장합니다.
-
-python get_similarity.py
-python topk_ensemble.py --weight=[weight1, weight2, ...]
 ```
+
+### get_similarity.py
+```bash
+python get_similarity.py
+```
+
+### topk_with_weight.py
+```bash
+python topk_with_weight.py --weight=[weight1, weight2, ...]
+```
+
+### topk_without_weight.py
+두 파일을 앙상블합니다.
+main 모델과 sub 모델을 파일명을 입력하고 main 모델에서 사용할 item 개수를 지정합니다.
+```bash
+python topk_without_weight.py --main_file MAIN_FILE --sub_file SUB_FILE --using_topk USING_TOPK
+```
+
+### split_by_interaction.py
+두 파일을 앙상블합니다.
+coldstart에 강한 모델을 less_file에, 상호작용이 많을 경우 잘 동작하는 모델을 much_file에 지정하고 상호작용 횟수를 지정합니다.
+### 주의사항
+전체 훈련 데이터를 기준으로 유저를 나누므로 기준이되는 훈련 파일을 지정해야 합니다.
+기본 설정은 우리 프로젝트의 구조에 주어진 데이터 파일을 넣은 위치입니다!
+
+```bash
+python split_by_interaction.py --train_path TRAIN_PATH --file_path FILE_PATH --less_file LESS_FILE --much_file MUCH_FILE --num_interaction NUM_INTERACTION
+```
+
+<br><br>
 
 0. submission 디렉토리에 저장할 csv 파일은 aistage에 제출하는 csv 파일과 동일한 형식이어야 합니다. 
 
@@ -45,8 +84,3 @@ python topk_ensemble.py --weight=[weight1, weight2, ...]
     2. 각 유저별로 추천된 아이템 Rank를 파일 별로 가중치를 주어 합합니다. 
 
     3. 합한 Rank를 다시 정렬하여 상위 10개의 아이템을 추천합니다.
-
-
-# More information
-
-작업중 ...
---
+++ src/Ensemble/split_by_interaction.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from collections import defaultdict
+import os
+import argparse
+import csv
+from datetime import datetime
+import pytz
+
+
+def korea_date_time():
+    """
+    Retrieves the current date and time in the Korea Standard Time (KST) timezone.
+
+    Returns:
+        str: The current date and time formatted as 'YYYY-MM-DD_HH:MM:SS' in KST.
+    """
+    korea_timezone = pytz.timezone("Asia/Seoul")
+    date_time = datetime.now(tz=korea_timezone)
+    date_time = date_time.strftime("%Y-%m-%d_%H:%M:%S")
+
+    return date_time
+
+
+def ensemble_interaction(
+    train_df: pd.DataFrame,
+    less_df: pd.DataFrame,
+    much_df: pd.DataFrame,
+    num_interaction: int,
+) -> None:
+    """
+    Perform ensemble interaction based on the number of interactions for each user,
+    concatenate the resulting DataFrames, and save the output to a CSV file.
+
+    Args:
+        train_df (pd.DataFrame): DataFrame containing training data.
+        less_df (pd.DataFrame): DataFrame containing results of models that behave well for data with less interaction.
+        much_df (pd.DataFrame): DataFrame containing results of models tthat behave well for data with a lot of interaction.
+        num_interaction (int): Threshold for the number of interactions to split the users.
+
+    Returns:
+        None
+    """
+
+    grouped = train_df.groupby("user").size().reset_index(name="num_inter")
+
+    split_under = grouped[grouped["num_inter"] <= num_interaction][
+        "user"
+    ].values.tolist()
+
+    split_over = grouped[grouped["num_inter"] > num_interaction]["user"].values.tolist()
+
+    less_df = less_df[less_df["user"].isin(split_under)]
+
+    much_df = much_df[much_df["user"].isin(split_over)]
+
+    output = pd.concat([less_df, much_df])
+    output.sort_index()
+
+    # Create the output folder
+    output_folder = "./for_ensemble"
+    os.makedirs(output_folder, exist_ok=True)
+
+    date_time = korea_date_time()
+    file_name = f"{output_folder}/ensemble-{date_time}.csv"
+    output.to_csv(file_name, index=False)
+
+    print(f"{file_name} is successfully saved!")
+
+
+def main(args):
+    train_df = pd.read_csv(args.train_path)
+    less_df = pd.read_csv(f"{args.file_path}{args.less_file}")
+    much_df = pd.read_csv(f"{args.file_path}{args.much_file}")
+
+    ensemble_interaction(
+        train_df=train_df,
+        less_df=less_df,
+        much_df=much_df,
+        num_interaction=args.num_interaction,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--train_path", type=str, default="../../data/train/train_ratings.csv"
+    )
+    parser.add_argument("--file_path", type=str, default="for_ensemble/")
+    parser.add_argument("--less_file", type=str, default="output1.csv")
+    parser.add_argument("--much_file", type=str, default="output2.csv")
+    # 유저를 절반: 114, 상호작용 횟수 분포 25%: 265,50%: 499,75%: 749
+    parser.add_argument("--num_interaction", type=int, default=114)
+
+    args = parser.parse_args()
+    main(args)
